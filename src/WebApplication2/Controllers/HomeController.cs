af5af4c861fb486b70103e43671551887644e096
Tina <Tina@FUNDAMENTALSERV>
2016-11-21 10:56:36 +0800

    Add project files.

---
+++ src/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+#if NET45
+using System.Drawing;
+#endif
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+#if NET45
+ System.Drawing.Bitmap a=new Bitmap();
+#endif
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
